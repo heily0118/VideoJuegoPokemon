18b2daf161ad19eef7fa1fda42247398dc892f19
Heily Rios <heilyayala005@gmail.com>
2025-03-28 21:34:37 -0500

    Se crearon las clases Pikachu y Squirtle

---
+++ src/autonoma/VideoJuegoPokemon/models/Squirtle.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.VideoJuegoPokemon.models;
+
+/**
+ *
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ */
+public class Squirtle extends Pokemon {
+
+    public Squirtle(int nombrePokedex, String nombre, double peso, int temporada) {
+        super(nombrePokedex, nombre, peso, temporada);
+    }
+    
+    
+    
+}
