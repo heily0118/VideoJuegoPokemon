03336aed165372a2deef89f9ae61c34a2cd1470a
María Paz Puerta Acevedo <mariapazpuerta@gmail.com>
2025-03-28 23:47:07 -0500

    Se agregó la documentación del código de la clase interface PokemonElectrico

--- src/autonoma/VideoJuegoPokemon/models/PokemonElectrico.java
+++ src/autonoma/VideoJuegoPokemon/models/PokemonElectrico.java
@@ -12,9 +12,28 @@ package autonoma.VideoJuegoPokemon.models;
  */
 public interface PokemonElectrico {
 
+    /**
+     * Realiza el ataque Impactrueno.
+     * @return Retorna un mensaje indicando que el Pokémon está utilizando el ataque Impactrueno.
+     */
     public abstract String atacarImpactrueno();
+    
+    /**
+     * Realiza el ataque Punio Trueno.
+     * @return Retorna un mensaje indicando que el Pokémon está utilizando el ataque Punio Trueno.
+     */
     public abstract String atacarPunioTrueno();
+    
+    /**
+     * Realiza el ataque Rayo.
+     * @return Retorna un mensaje indicando que el Pokémon está utilizando el ataque Rayo.
+     */
     public abstract String atacarRayo();
+    
+    /**
+     * Realiza el ataque Rayo Cargo.
+     * @return Retorna un mensaje indicando que el Pokémon está utilizando el ataque Rayo Cargo.
+     */
     public abstract String atacarRayoCargo();
     
 }
