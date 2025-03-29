fbc3e58379e537c62fbddbcfacd5718e36e2f2f9
Heily Rios <heilyayala005@gmail.com>
2025-03-28 22:16:40 -0500

    Se implementaron los metodos de la interfaz en pikachu

--- src/autonoma/VideoJuegoPokemon/models/Pikachu.java
+++ src/autonoma/VideoJuegoPokemon/models/Pikachu.java
@@ -6,8 +6,32 @@ package autonoma.VideoJuegoPokemon.models;
 
 /**
  *
- * @author USUARIO
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
  */
-public class Pikachu {
+public class Pikachu extends Pokemon implements PokemonElectrico {
     
+    public Pikachu(int nombrePokedex, String nombre, double peso, int temporada) {
+        super(nombrePokedex, nombre, peso, temporada);
+    }
+    
+
+    @Override
+    public String atacarPunioTrueno() {
+       return "Soy " + getNombre() + " y estoy atacando con Puño Trueno.";
+    }
+
+    @Override
+    public String atacarRayo() {
+        return "Soy " + getNombre() + " y estoy atacando con Rayo.";
+    }
+
+    @Override
+    public String atacarRayoCargo() {
+        return "Soy " + getNombre() + " y estoy atacando con Rayo Cargo.";
+    }
+
+    @Override
+    public String atacarImpacTrueno() {
+        return "Soy " + getNombre() + " y estoy atacando con Impac Trueno.";
+    }
 }
