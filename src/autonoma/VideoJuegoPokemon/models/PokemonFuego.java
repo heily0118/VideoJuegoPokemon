913bd5ee34ffd2afad2f60be45a46c347670edd3
Heily Rios <heilyayala005@gmail.com>
2025-03-28 21:42:21 -0500

    Se crearon 2 interfaces

---
+++ src/autonoma/VideoJuegoPokemon/models/PokemonFuego.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.VideoJuegoPokemon.models;
+
+/**
+ *
+ * @author Heily YOhana Rios Ayala <heilyy.riosa@autonoma.edi.co>
+ */
+public interface PokemonFuego {
+    
+    
+}
