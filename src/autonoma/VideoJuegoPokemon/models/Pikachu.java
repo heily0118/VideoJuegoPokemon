1a22b0e16546512f013207a19a2b0902e060e82f
María Paz Puerta Acevedo <mariapazpuerta@gmail.com>
2025-03-28 23:37:13 -0500

    Se agregó la documentación del código de la clase Pikachu

--- src/autonoma/VideoJuegoPokemon/models/Pikachu.java
+++ src/autonoma/VideoJuegoPokemon/models/Pikachu.java
@@ -12,26 +12,49 @@ package autonoma.VideoJuegoPokemon.models;
  */
 public class Pikachu extends Pokemon implements PokemonElectrico {
     
+    /**
+     * Constructor de la clase Pikachu.
+     * @param nombrePokedex El número de la Pokédex asignado a el pokémon Pikachu.
+     * @param nombre El nombre del pokémon (Pikachu).
+     * @param peso El peso del pokémon Pikachu.
+     * @param temporada La temporada en la que aparece el pokémon Pikachu.
+     * 
+     */
     public Pikachu(int nombrePokedex, String nombre, double peso, int temporada) {
         super(nombrePokedex, nombre, peso, temporada);
     }
     
-
+    /**
+     * El método se utiliza para representar el ataque Punio Trueno.
+     * @return Retorna el mensaje de quién es el pokémon (Pikachu) y que ataque esta utilizando (Punio Trueno).
+     */
     @Override
     public String atacarPunioTrueno() {
        return "Soy " + getNombre() + " y estoy atacando con Punio Trueno.";
     }
 
+    /**
+     * El método se utiliza para representar el ataque Rayo.
+     * @return Retorna el mensaje de quién es el pokémon (Pikachu) y que ataque esta utilizando (Rayo).
+     */
     @Override
     public String atacarRayo() {
         return "Soy " + getNombre() + " y estoy atacando con Rayo.";
     }
 
+    /**
+     * El método se utiliza para representar el ataque Rayo Cargo.
+     * @return Retorna el mensaje de quién es el pokémon (Pikachu) y que ataque esta utilizando (Rayo Cargo).
+     */
     @Override
     public String atacarRayoCargo() {
         return "Soy " + getNombre() + " y estoy atacando con Rayo Cargo.";
     }
 
+    /**
+     * El método se utiliza para representar el ataque ImpacTrueno.
+     * @return Retorna el mensaje de quién es el pokémon (Pikachu) y que ataque esta utilizando (ImpacTrueno).
+     */
     @Override
     public String atacarImpactrueno() {
         return "Soy " + getNombre() + " y estoy atacando con Impactrueno.";
