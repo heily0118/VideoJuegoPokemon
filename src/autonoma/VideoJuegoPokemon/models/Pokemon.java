f6b319380f474d7efdb889876c13b65bb788a12b
Heily Rios <heilyayala005@gmail.com>
2025-03-28 21:28:47 -0500

    Se implementaron los atributos y metodos de Pokemon

--- src/autonoma/VideoJuegoPokemon/models/Pokemon.java
+++ src/autonoma/VideoJuegoPokemon/models/Pokemon.java
@@ -9,5 +9,24 @@ package autonoma.VideoJuegoPokemon.models;
  * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
  */
 public abstract class Pokemon {
-    
+    private int nombrePokedex;
+    private String nombre;
+    private double peso;
+    private int temporada;
+
+    public Pokemon(int nombrePokedex, String nombre, double peso, int temporada) {
+        this.nombrePokedex = nombrePokedex;
+        this.nombre = nombre;
+        this.peso = peso;
+        this.temporada = temporada;
+    }
+    public void atacarPlacaje(){
+        
+    }
+    public void atacarArañazo(){
+        
+    }
+    public void atacarMordisco(){
+        
+    }
 }
